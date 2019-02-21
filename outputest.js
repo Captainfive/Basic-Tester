297c73788006399a5d392e724eb181042131bf48
Captainfive <vinou_montes@hotmail.fr>
2019-02-21 02:07:50 +0100

    Script test

---
+++ outputest.js
@@ -0,0 +1,25 @@
+function add(a , b) {
+    return a + b
+}
+
+function hello(yop) {
+    return yop
+}
+
+function nul() {
+    return null
+}
+
+function undef(){
+    return undefined
+}
+
+function promis(){
+    return Promise
+}
+
+function name(parameters){
+    statements
+  }
+
+module.exports = { add, hello, nul, undef, promis, name }
