73ef0094de9d4564c1a0a98452d55e9b1d93d5f9
Captainfive <vinou_montes@hotmail.fr>
2019-02-23 06:41:34 +0100

    adding new taggedstring

--- src/Tagged.js
+++ src/Tagged.js
@@ -9,8 +9,13 @@ const AvaStart = {
 const TestPartsFunction = {
     avaStartFunction: taggedString`avaTest("Function ${0}", (assert) => {\n`,
     avaIsFunction: taggedString`    assert.true(is.func(${0}))\n`,
-    avaFunctionReturnNumber: taggedString`  assert.true(is.number(${0}(10)))\n`,
-    avaFunctionReturnString: taggedString`  assert.true(is.string(${0}()))\n`,
+    avaFunctionReturnNumber: taggedString`    assert.true(is.number(${0}(10)))\n`,
+    avaFunctionReturnString: taggedString`    assert.true(is.string(${0}()))\n`,
+    avaFunctionReturnObject: taggedString`    assert.true(is.nullValue(${0}))\n`,
+    avaFunctionReturnPlainObject: taggedString`    assert.true(is.plainObject(${0}()))\n`,
+    avaFunctionReturnPromise: taggedString`    assert.true(is.promise(${0}()))\n`,
+    avaFunctionReturnArray: taggedString`    assert.true(is.array(${0}()))\n`,
+    avaFunctionReturnClass: taggedString`    assert.true(is.classObject(new ${0}()))\n`,
     avaEND: `});\n`,
 
 };
