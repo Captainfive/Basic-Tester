b33b37e4b8b90d4f670eef7a8dbff5da105f33df
Captainfive <vinou_montes@hotmail.fr>
2019-02-23 01:22:01 +0100

    Function, return number,string

--- ClassTest.js
+++ ClassTest.js
@@ -1,25 +1,41 @@
+// Require NodeJS Dependencies
+const { appendFile } = require("fs").promises
+
+// Helpers 
+const { AvaStart } = require("./src/Tagged")
+const { TestPartsFunction } = require("./src/Tagged")
+
 class Test {
     constructor(ScriptPath, ScriptParsed) {
         this.ScriptPath = ScriptPath
         this.ScriptParsed = ScriptParsed
     }
 
-    // async TestFonction(Scriptparsed) {
-    //     for (const obj of Scriptparsed.body) {
-    //         if(obj.)
-    //     }
-    // }
-
-    CheckFunctions(Scriptparsed){
+    async CheckFunctions(Scriptparsed) {
+        // Constants
+        const RC = await appendFile("test.js", "\n")
         let arr = []
         for (const obj of Scriptparsed.body) {
             if (obj.type === "FunctionDeclaration") {
-                console.log(obj);
-                console.log("---\n");
-                console.log(obj.body.body[0]);
-                console.log("---\n");
-                arr.push(obj.id.name)
-                arr.push(obj.body.body[0].argument.type)
+                if (obj.body.body[0].type === "ReturnStatement") {
+                    if (obj.body.body[0].argument.type === "Identifier") {
+                        await appendFile("test.js", `${TestPartsFunction.avaStartFunction(obj.id.name)}`)
+                        await appendFile("test.js", `${TestPartsFunction.avaIsFunction(obj.id.name)}`)
+                        await appendFile("test.js", `${TestPartsFunction.avaEND}`)
+                        await RC
+                    }
+                    if (obj.body.body[0].argument.type === "Literal") {
+                        await appendFile("test.js", `${TestPartsFunction.avaStartFunction(obj.id.name)}`)
+                        if (typeof (obj.body.body[0].argument.value) === "string") {
+                            await appendFile("test.js", `${TestPartsFunction.avaFunctionReturnString(obj.id.name)}`)
+                        }
+                        if (typeof (obj.body.body[0].argument.value) === "number") {
+                            await appendFile("test.js", `${TestPartsFunction.avaFunctionReturnNumber(obj.id.name)}`)
+                        }
+                        await appendFile("test.js", `${TestPartsFunction.avaEND}`)
+                        await RC
+                    }
+                }
             }
         }
         return arr
