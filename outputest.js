2bbf735ca4d796822f67d33ef812df1c704a8f7e
Captainfive <vinou_montes@hotmail.fr>
2019-02-23 06:39:25 +0100

    adding more exemple

--- outputest.js
+++ outputest.js
@@ -1,4 +1,4 @@
-function add(a) {
+function func(a) {
     return a
 }
 
@@ -6,9 +6,36 @@ function nombre() {
     return 2
 }
 
-function yop() {
+function string() {
     return "hello"
 }
 
+function object() {
+    return Object.create(null)
+}
 
-module.exports = { add, nombre, yop }
+function Promis() {
+    return new Promise(function(res, rej) {
+        res("hello")
+    })
+}
+
+function array() {
+    return []
+}
+
+function classes() {
+    return class Rectangle {
+        constructor(hauteur, largeur) {
+          this.hauteur = hauteur;
+          this.largeur = largeur;
+        }
+      };
+}
+
+function obj() {
+    return '{}'
+}
+
+module.exports = { func, nombre, string, object,
+ Promis, array, classes, obj }
