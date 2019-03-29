64539bd334f66da1f72ca6f10c266277df20ca0b
Captainfive <vinou_montes@hotmail.fr>
2019-03-29 16:21:47 +0100

    chore: adding Array, Class, Object to auto-test

--- ClassAlgo.js
+++ ClassAlgo.js
@@ -4,13 +4,13 @@ const { appendFile } = require("fs").promises
 // Helpers 
 const { TestPartsFunction } = require("./src/Tagged")
 
-class Algo{
+class Algo {
     constructor(ScriptPath, ScriptParsed) {
         this.ScriptPath = ScriptPath
         this.ScriptParsed = ScriptParsed
     }
 
-    async write(TaggedString, FunctionTested){
+    async write(TaggedString, FunctionTested) {
         await appendFile("test.js", `${TestPartsFunction.avaStartFunction(FunctionTested)}`)
         await appendFile("test.js", `${TestPartsFunction.avaIsFunction(FunctionTested)}`)
         await appendFile("test.js", `${TaggedString}`)
@@ -18,7 +18,7 @@ class Algo{
     }
 
     CheckModuleExports(Scriptparsed) {
-        if (typeof Scriptparsed !== "object"){
+        if (typeof Scriptparsed !== "object") {
             throw new TypeError("Scriptparsed should be typeof object!");
         }
 
@@ -40,33 +40,43 @@ class Algo{
         }
         return arr
     }
-    
+
     async CheckFunctions(Scriptparsed) {
         // Constants
         const RC = await appendFile("test.js", "\n")
         let arr = []
         for (const obj of Scriptparsed.body) {
-            if (obj.type === "FunctionDeclaration") {
-                if (obj.body.body[0].type === "ReturnStatement") {
-                    if (obj.body.body[0].argument.type === "ObjectExpression") {
+            if (Reflect.get(obj, "type") === "FunctionDeclaration") {
+                if (Reflect.get(obj.body.body[0], "type") === "ReturnStatement") {
+                    if (Reflect.get(obj.body.body[0].argument, "type") === "ObjectExpression") {
                         await this.write(TestPartsFunction.avaFunctionReturnObject(obj.id.name), obj.id.name)
                     }
-                    if (obj.body.body[0].argument.type === "ClassExpression") {
+                    if (Reflect.get(obj.body.body[0].argument, "type") === "ClassExpression") {
                         await this.write(TestPartsFunction.avaFunctionReturnClass(obj.id.name), obj.id.name)
                     }
-                    if (obj.body.body[0].argument.type === "ArrayExpression") {
-                        if (obj.body.body[0].argument.elements.length === 0){
+                    if (Reflect.get(obj.body.body[0].argument, "type") === "ArrayExpression") {
+                        if (obj.body.body[0].argument.elements.length === 0) {
                             await this.write(TestPartsFunction.avaFunctionReturnArray(obj.id.name), obj.id.name)
                         }
                     }
-                    if (obj.body.body[0].argument.type === "NewExpression") {
-                        // await this.write(TestPartsFunction.avaFunctionReturnObject(obj.id.name), obj.id.name)
+                    if (Reflect.get(obj.body.body[0].argument, "type") === "NewExpression") {
+                        if (Reflect.get(obj.body.body[0].argument.callee, "name") === "Promise") {
+                            await this.write(TestPartsFunction.avaFunctionReturnPromise(obj.id.name), obj.id.name)
+                        }
+                        if (Reflect.get(obj.body.body[0].argument.callee, "name") === "Set") {
+                            await this.write(TestPartsFunction.avaFunctionReturnSet(obj.id.name), obj.id.name)
+                        }
+                        if (Reflect.get(obj.body.body[0].argument.callee, "name") === "Map") {
+                            await this.write(TestPartsFunction.avaFunctionReturnMap(obj.id.name), obj.id.name)
+                        }
+                        if (Reflect.get(obj.body.body[0].argument.callee, "name") === "WeakMap") {
+                            await this.write(TestPartsFunction.avaFunctionReturnWeakMap(obj.id.name), obj.id.name)
+                        }
                     }
-                    console.log(obj.body.body[0]);
-                    if (obj.body.body[0].argument.type === "Identifier") {
+                    if (Reflect.get(obj.body.body[0].argument, "type") === "Identifier") {
                         await this.write(TestPartsFunction.avaFunctionReturnObject(obj.id.name), obj.id.name)
                     }
-                    if (obj.body.body[0].argument.type === "Literal") {
+                    if (Reflect.get(obj.body.body[0].argument, "type") === "Literal") {
                         await appendFile("test.js", `${TestPartsFunction.avaStartFunction(obj.id.name)}`)
                         if (typeof (obj.body.body[0].argument.value) === "string") {
                             await appendFile("test.js", `${TestPartsFunction.avaFunctionReturnString(obj.id.name)}`)
@@ -77,7 +87,7 @@ class Algo{
                         await appendFile("test.js", `${TestPartsFunction.avaEND}`)
                         await RC
                     }
-                    if (obj.body.body[0].argument.type === "CallExpression") {
+                    if (Reflect.get(obj.body.body[0].argument, "type") === "CallExpression") {
                         if (obj.body.body[0].argument.callee.object.name === "Object") {
                             await this.write(TestPartsFunction.avaFunctionReturnObject(obj.id.name), obj.id.name)
                         }
@@ -87,5 +97,5 @@ class Algo{
         }
         return arr
     }
-} 
+}
 module.exports = { Algo }
