d444df23b61bba3143c7d05fc3176f049158e827
Captainfive <vinou_montes@hotmail.fr>
2019-04-04 06:20:11 +0200

    chore: add bool, null, weakset, buffer, date, regexp, error to tagged string

--- src/Tagged.js
+++ src/Tagged.js
@@ -2,7 +2,7 @@ const { taggedString } = require("@slimio/utils");
 
 // Start Declarations
 const AvaStart = {
-    import: taggedString`const { ${0} } = require("./${1}");\n`,
+    import: taggedString`const {${0} } = require("./${1}");\n`,
     require: taggedString`const ${0} = require("${1}");\n`,
 }
 
@@ -12,16 +12,22 @@ const TestPartsFunction = {
     avaIsFunction: taggedString`    assert.true(is.func(${0}))\n`,
     avaFunctionReturnNumber: taggedString`    assert.true(is.number(${0}(10)))\n`,
     avaFunctionReturnString: taggedString`    assert.true(is.string(${0}()))\n`,
+    avaFunctionReturnBoolean: taggedString`    assert.true(is.boolean(${0}()))\n`,
+    avaFunctionReturnNull: taggedString`    assert.true(is.nullValue(${0}()))\n`,
     avaFunctionReturnObject: taggedString`    assert.true(is.object(${0}))\n`,
     avaFunctionReturnPlainObject: taggedString`    assert.true(is.plainObject(${0}()))\n`,
     avaFunctionReturnPromise: taggedString`    assert.true(is.promise(${0}()))\n`,
     avaFunctionReturnArray: taggedString`    assert.true(is.array(${0}()))\n`,
     avaFunctionReturnClass: taggedString`    assert.true(is.classObject(new ${0}()))\n`,
     avaFunctionReturnSet: taggedString`    assert.true(is.set(new ${0}()))\n`,
+    avaFunctionReturnWeakSet: taggedString`    assert.true(is.weakSet(new ${0}()))\n`,
     avaFunctionReturnMap: taggedString`    assert.true(is.map(new ${0}()))\n`,
     avaFunctionReturnWeakMap: taggedString`    assert.true(is.weakMap(new ${0}()))\n`,
     avaFunctionReturnBigint: taggedString`    assert.true(is.bigint(${0}()))\n`,
+    avaFunctionReturnBuffer: taggedString`    assert.true(is.buffer(${0}()))\n`,
+    avaFunctionReturnDate: taggedString`    assert.true(is.date(${0}()))\n`,
+    avaFunctionReturnRegExp: taggedString`    assert.true(is.regExp(${0}()))\n`,
+    avaFunctionReturnError: taggedString`    assert.true(is.error(${0}()))\n`,
     avaEND: `});\n\n`,
-
 };
 module.exports = { AvaStart, TestPartsFunction }
